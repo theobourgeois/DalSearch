8aed3a59478d90c57f49b2ee7a1ebc5581d25683
theobourgeois <theoobourgeois@gmail.com>
2024-11-25 12:30:19 -0400

    fix type errors

--- src/app/[course]/page.tsx
+++ src/app/[course]/page.tsx
@@ -1,26 +1,24 @@
-import courses from '../search.json'
+import courses from "../search.json";
 
-type CourseKey = keyof typeof courses
+type CourseKey = keyof typeof courses;
 
-export default function Page({ params }: { params: { course: CourseKey } }) {
-  const course = courses[params.course]
+export default function Page({ params }: { params: { course: string } }) {
+  const course = courses[params.course as CourseKey];
   if (!course) {
-    return <h1>Course not found</h1>
+    return <h1>Course not found</h1>;
   }
 
   return (
     <main>
       <section>
-        <h1>{course.subjectCode} {course.courseCode} - {course.title}</h1>
+        <h1>
+          {course.subjectCode} {course.courseCode} - {course.title}
+        </h1>
         <p>{course.description}</p>
         {course.prerequisites.map((prereq) => (
           <p key={prereq}>{prereq}</p>
         ))}
       </section>
     </main>
-  )
-
-
-
-
+  );
 }
