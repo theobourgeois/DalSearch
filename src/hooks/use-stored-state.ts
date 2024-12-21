c16b7bac84368ea7bc87956e861b2c275ab4ff90
theobourgeois <theoobourgeois@gmail.com>
2024-12-21 18:52:32 -0400

    eslint errors

--- src/hooks/use-stored-state.ts
+++ src/hooks/use-stored-state.ts
@@ -1,17 +1,19 @@
 "use client";
 import { useEffect, useState } from "react";
 
+let localStorage: Storage | null = null;
+
 // This hook is used to store a state in the local storage
 export function useStoredState<T>(defaultValue: T, key: string) {
     const [state, setState] = useState<T>(() => {
-        const storedState = localStorage.getItem(key);
+        const storedState = localStorage?.getItem(key);
         return storedState && storedState !== "undefined"
             ? JSON.parse(storedState)
             : defaultValue;
     });
 
     useEffect(() => {
-        localStorage.setItem(key, JSON.stringify(state));
+        localStorage?.setItem(key, JSON.stringify(state));
     }, [state, key]);
 
     const set: typeof setState = (newState: T | ((prevState: T) => T)) => {
@@ -19,7 +21,7 @@ export function useStoredState<T>(defaultValue: T, key: string) {
             const resolvedState = typeof newState === "function"
                 ? (newState as (prev: T) => T)(prevState)
                 : newState;
-            localStorage.setItem(key, JSON.stringify(resolvedState));
+            localStorage?.setItem(key, JSON.stringify(resolvedState));
             return resolvedState;
         });
     };
