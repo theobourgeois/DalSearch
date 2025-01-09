e58da9694b479ad725b0c26f9a5af2ade28539aa
theobourgeois <theoobourgeois@gmail.com>
2025-01-08 22:18:08 -0400

    add endpoints for courses, filters, recommendation search

--- src/utils/course.ts
+++ src/utils/course.ts
@@ -103,3 +103,107 @@ export const terms = {
 export const subjects = subjectsData as Subject[];
 
 export const creditHours = Array.from(new Set(Object.values(courses).map((course) => Number(course.creditHours)))).sort((a, b) => a - b);
+
+const NUM_COURSE_LEVELS = 9;
+const ALL_COURSE_LEVELS = Array.from({ length: NUM_COURSE_LEVELS }, (_, i) =>
+  ((i + 1) * 1000).toString()
+);
+
+export const defaultFilter: CourseFilter = {
+  terms: Object.keys(terms) as Term[],
+  courseLevels: ALL_COURSE_LEVELS,
+  subjectCodes: subjects.map((subject) => subject.code),
+  searchTerm: "",
+  creditHours,
+};
+
+export const defaultOrderBy: CourseOrderBy = {
+  key: "title",
+  direction: "asc",
+};
+
+
+export function getFilteredCourses(
+  courses: Course[],
+  filter: CourseFilter,
+  orderBy: CourseOrderBy,
+  maxNumCourses: number
+) {
+  const keywordFilteredCourses = courses.filter((course) => {
+    const keyword = filter.searchTerm.toLowerCase();
+    return (
+      course.title.toLowerCase().includes(keyword) ||
+      course.subjectCode.toLowerCase().includes(keyword) ||
+      course.courseCode.toLowerCase().includes(keyword)
+    );
+  });
+
+  const filteredCourses = keywordFilteredCourses.filter((course) => {
+    const hasSelectedTerm = course.termClasses.some((termClass) => {
+      return filter.terms.includes(termClass.term);
+    });
+    const hasSelectedCourseLevel = filter.courseLevels.includes(
+      course.courseCode[0] + "000"
+    );
+    const hasSelectedSubjectCode = filter.subjectCodes.includes(
+      course.subjectCode
+    );
+    const hasSelectedCreditHours = filter.creditHours.includes(
+      course.creditHours
+    );
+    return (
+      hasSelectedTerm &&
+      hasSelectedCourseLevel &&
+      hasSelectedCreditHours &&
+      hasSelectedSubjectCode
+    );
+  });
+
+  // order by
+
+  switch (orderBy.key) {
+    case "courseCode":
+      if (orderBy.direction === "asc") {
+        filteredCourses.sort((a, b) =>
+          (a.subjectCode + a.subjectCode).localeCompare(
+            b.subjectCode + b.subjectCode
+          )
+        );
+      } else {
+        filteredCourses.sort((a, b) =>
+          (b.subjectCode + b.subjectCode).localeCompare(
+            a.subjectCode + a.subjectCode
+          )
+        );
+      }
+      break;
+    case "title":
+      if (orderBy.direction === "asc") {
+        filteredCourses.sort((a, b) => a.title.localeCompare(b.title));
+      } else {
+        filteredCourses.sort((a, b) => b.title.localeCompare(a.title));
+      }
+      break;
+    case "creditHours":
+      if (orderBy.direction === "asc") {
+        filteredCourses.sort((a, b) => a.creditHours - b.creditHours);
+      } else {
+        filteredCourses.sort((a, b) => b.creditHours - a.creditHours);
+      }
+      break;
+    case "numClasses":
+      if (orderBy.direction === "asc") {
+        filteredCourses.sort(
+          (a, b) => a.termClasses.length - b.termClasses.length
+        );
+      } else {
+        filteredCourses.sort(
+          (a, b) => a.termClasses.length - b.termClasses.length
+        );
+      }
+      break;
+  }
+
+  return filteredCourses.slice(0, maxNumCourses);
+}
+
